f0e2f6b097408dffa7248b7121500ccdf55cdf25
Santhosh20021995 <84032564+Santhosh20021995@users.noreply.github.com>
2021-06-21 22:15:57 +0530

    Add files via upload

---
+++ xxx.js
@@ -0,0 +1,34 @@
+var photo0 = document.getElementById("img0");
+console.log(photo0);
+var photo1 = document.getElementById("img1");
+console.log(photo1);
+var photo2 = document.getElementById("img2");
+console.log(photo2);
+var photo3 = document.getElementById("img3");
+console.log(photo3);  
+var photo4 = document.getElementById("img4");
+console.log(photo4);
+var photo5 = document.getElementById("img5");
+console.log(photo5);
+var productImg = document.getElementById("productImg");
+console.log(productImg);
+
+photo0.addEventListener("click", function(){
+  productImg.src = "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg"
+});
+photo1.addEventListener("click", function(){
+  productImg.src = "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg"
+});
+photo2.addEventListener("click", function(){
+  productImg.src = "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/efc3d5b9-1bb3-4427-af53-7acae7af98951541402833591-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-2.jpg"
+});
+photo3.addEventListener("click", function(){
+  productImg.src = "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/c7e58861-3431-4189-9903-9880f5eebd181541402833566-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-3.jpg"
+});
+photo4.addEventListener("click", function(){
+  productImg.src = "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/66490b64-32de-44b4-a6e4-fe36f1c040051541402833548-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-4.jpg"
+});
+photo5.addEventListener("click", function(){
+  productImg.src = "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/957be784-7c5d-4e90-ab9f-0928015b22891541402833645-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-5.jpg"
+});
+
